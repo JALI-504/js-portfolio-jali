d10e72cebcc5c94580f90483f2f1e062fa1aaa94
JALI <furio504@hotmail.com>
2022-08-26 14:33:22 -0600

    Configuracion de Html en WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports ={
     entry:'./src/index.js',
@@ -19,5 +21,14 @@ module.exports ={
             }
         }
         ]
-    },
-}
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html'
+            })
+        
+        ]
+    
+    }
