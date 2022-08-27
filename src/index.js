58080e7037371ba23ef6408fab2f7d179aef78cd
JALI <furio504@hotmail.com>
2022-08-27 14:07:31 -0600

    Loader de Imagenes optimizadas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Template from './templates/Template.js';
 import './styles/main.css';
 import './styles/vars.styl';
 
+
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
