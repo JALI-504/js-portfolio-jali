fce4cc684854ff3b9bee02b481bc6ca46ba98e27
JALI <furio504@hotmail.com>
2022-08-28 15:18:11 -0600

    Webpack bundle y analyzer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
 
 module.exports ={
@@ -77,6 +78,7 @@ module.exports ={
                 ]
             }),
             new Dotenv(),
+            new BundleAnalyzerPlugin(),
         ],
         devServer: {
             static: {
