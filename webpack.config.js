f430ac3d23c17c5a85bebc7afcd20639a0e9a093
JALI <furio504@hotmail.com>
2022-08-27 18:35:42 -0600

    Deply del proyecto, variables de entorno

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 
 module.exports ={
@@ -74,7 +75,8 @@ module.exports ={
                         to: "assets/images"
                     }
                 ]
-            })
+            }),
+            new Dotenv(),
         ],
         optimization:{
                minimize: true,
