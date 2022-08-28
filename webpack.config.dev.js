b1ed7cb1590d02b0e9b0c1001f8b5e0107b6598c
JALI <furio504@hotmail.com>
2022-08-28 15:38:21 -0600

    agregar configuracion de DecTool

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 module.exports ={
@@ -14,6 +14,7 @@ module.exports ={
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development', 
+    devtool: 'source-map',
    
     resolve: {
         extensions: ['.js'],
