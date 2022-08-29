4a54438a6f8b8a7274b79908c86d47fe3b65b40b
JALI <furio504@hotmail.com>
2022-08-28 21:42:13 -0600

    Cambio de redes sociales

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -17,7 +17,7 @@ const Template = async () => {
               <circle cx="50" cy="50" r="40" />
             </svg>
           </div>
-          <p class="card_title">Hi, My name is</p>
+          <p class="card_title">Hola, Mi Nombre es:</p>
           <p class="card_value">${data.name.first} ${data.name.last}</p>
         </div>
         <div class="card_userdata">
@@ -27,13 +27,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://twitter.com/">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/JALI-504">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://instagram.com/">
             <img src="${instagram}" />
           </a>
         </div>
